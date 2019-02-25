e6215a58b2de92daefe0e40b46d4c8d77966f115
Dotan Simha <dotansimha@gmail.com>
2019-02-25 09:07:07 +0200

    WIP, move common visitor to separate package, clear flow package, added stub for ts

--- packages/plugins/typescript/src/index.ts
+++ packages/plugins/typescript/src/index.ts
@@ -1,31 +1,26 @@
 import { DocumentFile, PluginFunction } from 'graphql-codegen-core';
 import { parse, printSchema, visit, GraphQLSchema } from 'graphql';
-import { FlowVisitor } from './visitor';
 import { ScalarsMap, EnumValuesMap } from 'graphql-codegen-visitor-plugin-common';
+import { TsVisitor } from './visitor';
 
-export { DEFAULT_SCALARS } from 'graphql-codegen-visitor-plugin-common/src/scalars';
-export * from 'graphql-codegen-visitor-plugin-common/src/variables-to-object';
-export * from './visitor';
-
-export interface FlowPluginConfig {
+export interface TypeScriptPluginConfig {
   scalars?: ScalarsMap;
   enumValues?: EnumValuesMap;
   namingConvention?: string;
   typesPrefix?: string;
 }
 
-export const plugin: PluginFunction<FlowPluginConfig> = (
+export const plugin: PluginFunction<TypeScriptPluginConfig> = (
   schema: GraphQLSchema,
   documents: DocumentFile[],
-  config: FlowPluginConfig
+  config: TypeScriptPluginConfig
 ) => {
-  const result = `/* @flow */\n\n`;
   const printedSchema = printSchema(schema);
   const astNode = parse(printedSchema);
 
   const visitorResult = visit(astNode, {
-    leave: new FlowVisitor(config)
+    leave: new TsVisitor(config) as any
   });
 
-  return result + visitorResult.definitions.join('\n');
+  return visitorResult.definitions.join('\n');
 };
